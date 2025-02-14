6860932d29f6e2e6a4c4e2a252b596a5bdc087ce
prog6 <prog6@team74>
2025-02-13 19:25:53 -0500

    Drive forward auton

---
+++ src/main/java/frc/robot/driveForwardAuton.java
@@ -0,0 +1,74 @@
+package frc.robot;
+
+import com.revrobotics.spark.SparkMax;
+import com.studica.frc.AHRS;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.testrobotautonmovement.RobotMovement.Motor;
+
+public class driveForwardAuton {
+    SwerveModule rightFront;
+    SwerveModule rightBack;
+    SwerveModule leftFront;
+    SwerveModule leftBack;
+    SparkMax liftMotor;
+    String state = "start";
+    SwerveDriveKinematics drive1;
+    AHRS gyro1;
+
+    public driveForwardAuton(SwerveModule[] List, SparkMax Lift, SwerveDriveKinematics drive, AHRS gyro) {
+        rightFront = List[0];
+        rightBack = List[1];
+        leftFront = List[2];
+        leftBack = List[3];
+        liftMotor = Lift;
+        drive1 = drive;
+        gyro1 = gyro;
+    }
+
+    void RunS2R(int time) {
+        switch (state) {
+            // first number in if statement is time in seconds
+            case "start":
+            MotorSet(0.2,0.0,0.0);
+            if (time > 100) {
+                state = "stop";
+            }
+            break;
+
+            case "stop":
+            MotorSet(0.0,0.0,0.0);
+            break;
+
+        }
+    }
+
+
+
+    void MotorSet(double Y, double X, double rotation){
+    
+        ChassisSpeeds control = ChassisSpeeds.fromFieldRelativeSpeeds(Y, X, rotation, gyro1.getRotation2d());
+        SwerveModuleState[] moduleStates = drive1.toSwerveModuleStates(control);
+
+  moduleStates[0].optimize(Rotation2d.fromDegrees(rightFront.getRotation()));
+  moduleStates[1].optimize(Rotation2d.fromDegrees(leftFront.getRotation()));
+  moduleStates[2].optimize(Rotation2d.fromDegrees(rightBack.getRotation()));
+  moduleStates[3].optimize(Rotation2d.fromDegrees(leftBack.getRotation()));
+  
+  rightFront.turny(moduleStates[0].angle.getDegrees());
+  leftFront.turny(moduleStates[1].angle.getDegrees());
+  rightBack.turny(moduleStates[2].angle.getDegrees());
+  leftBack.turny(moduleStates[3].angle.getDegrees());
+
+
+    rightFront.movey(moduleStates[0].speedMetersPerSecond);
+    leftFront.movey(moduleStates[1].speedMetersPerSecond);
+    rightBack.movey(moduleStates[2].speedMetersPerSecond);
+    leftBack.movey(moduleStates[3].speedMetersPerSecond);
+
+
+    }
+}
