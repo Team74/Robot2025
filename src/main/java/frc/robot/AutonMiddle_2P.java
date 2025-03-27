430780cb65886501a199c13009d78bc03a35a063
mattbialko <51932848+mattbialko@users.noreply.github.com>
2025-03-27 18:30:47 -0400

    sample reef code

--- src/main/java/frc/robot/AutonMiddle_2P.java
+++ src/main/java/frc/robot/AutonMiddle_2P.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class AutonMiddle_2P {
@@ -183,4 +184,89 @@ public class AutonMiddle_2P {
         return new Object[]{currentState, time};
     }
 
+
+    Object[] Run_ReefToPlayer(Object[] autoState, double getPeriod) {
+        String currentState = autoState[0].toString();
+
+        if(time < 20) {
+        driveTrain.driveLL(0.5, 0, 0, false, getPeriod);
+        }
+        if(time > 20 && time < 100) {
+        driveTrain.drive(0, 0, 0, false, false);
+        }
+        if(time > 100 && time < 200) {
+        var rot = driveTrain.getTurnBotToAngle(-21);
+        rot = MathUtil.clamp(rot, -0.6, 0.6);
+
+        System.out.println("rot: " + rot);
+        driveTrain.drive(0, 0, rot, false, false);
+        }
+        if(time > 200 && time < 300) {
+        driveTrain.drive(0, 0, 0, false, false);
+        }
+        if(time > 300 && time < 600) {
+        var April_12 = driveTrain.GetAprilTagTelemotry(12);
+        var rot = driveTrain.getTurnBotToAngle(-21);
+
+        if(April_12 != null) {
+            limelightcam.LimeTargetWithRot(getPeriod, rot);
+        }
+        else {
+            driveTrain.driveLL(-0.5, 0, rot, false, getPeriod);
+        }
+        }
+        if(time > 600 && time < 700) {
+        var rot = driveTrain.getTurnBotToAngle(135);
+        driveTrain.driveLL(0, 0, rot, false, getPeriod);
+        }
+        if(time > 700 && time < 750) {
+        var rot = driveTrain.getTurnBotToAngle(135);
+        driveTrain.driveLL(0.5, 0, rot, false, getPeriod);
+        }
+        //Set the rotation 
+        //Align to center of 17
+        if(time > 750 && time < 850) {
+        var rot = driveTrain.getTurnBotToAngle(135);
+        double rotationOutput = limelightcam.LLGetRotation();
+
+        driveTrain.driveLL(0, -rotationOutput, rot, false, getPeriod);
+        }
+        //Back up towards 12
+        if(time > 850 && time < 900) {
+        var April_17 = driveTrain.GetAprilTagTelemotry(17);
+        var rot = driveTrain.getTurnBotToAngle(135);
+
+        if(April_17 != null) {
+            double rangeOutput = limelightcam.LLGetRangeOutput();
+            
+            rangeOutput = MathUtil.clamp(rangeOutput, -1, 1);
+
+            if(rangeOutput < -3.6) { 
+            rangeOutput = MathUtil.clamp(rangeOutput, -0.3, 0.3);
+            }
+
+            driveTrain.driveLL(-rangeOutput, -0.5, rot, false, getPeriod);
+
+            if(rangeOutput < -3.8) { 
+            time = 900;
+            }
+
+        }
+        else {
+            //driveTrain.driveLL(0.5, 0, rot, false, getPeriod());
+        }
+        }
+        if(time > 900 && time < 1000) {
+        var rot = driveTrain.getTurnBotToAngle(135);
+
+        driveTrain.driveLL(0, -0.5, rot, false, getPeriod);
+        }
+
+        if(time > 1000) {
+        driveTrain.drive(0, 0, 0, false, false);
+        }
+        time++;
+        return new Object[]{currentState, time};
+
+    }
 }
